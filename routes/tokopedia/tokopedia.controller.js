748cf2088d4fc2c37684aa04a518bfecd2b47ef8
Vinzzztty <kevinarnandes21@gmail.com>
2024-04-07 20:47:29 +0700

    init scrape tokopedia

--- routes/tokopedia/tokopedia.controller.js
+++ routes/tokopedia/tokopedia.controller.js
@@ -23,29 +23,74 @@ const crawlTokped = async (req, res) => {
             args: ["--no-sandbox", "--disable-setuid-sandbox"],
         });
 
-        // const context = await browser.createIncognitoBrowserContext(); //mode penyamaran
-
         // open a new page
         const page = await browser.newPage();
 
         await page.setJavaScriptEnabled(true); //aktifkan javascript
         await page.setUserAgent(randomAgent); //setting user agent
         await page.goto(url, { waituntil: "domcontentloaded", timeout: 0 }); //tunggu proses dom/load pagenya selesai
-        const body = await page.evaluate(() => {
-            return document.querySelector("body").innerHTML;
-        }); //mendapatkan isi tag html body
 
-        console.log(body);
-
-        // Respond with a success message
-        res.status(200).json({
-            message: "Website is accessible and can be scraped",
-            data: body,
+        // Delay for 2 seconds to ensure the page content is fully loaded
+        await page.evaluate(() => {
+            return new Promise((resolve) => setTimeout(resolve, 5000));
         });
+
+        const articleHTML = await page.evaluate(() => {
+            const articles = Array.from(
+                document.querySelectorAll("article.css-72zbc4")
+            );
+
+            const reviewArticle = [];
+
+            // Counter to limit the number of articles to 50
+            let count = 0;
+
+            articles.forEach((article) => {
+                if (count >= 50) return; // Exit loop if we have reached 50 articles
+
+                const user = article.querySelector(".name").textContent.trim();
+
+                // Check if the element exists before accessing its textContent
+                const reviewElement = article.querySelector(
+                    '[data-testid="lblItemUlasan"]'
+                );
+                const review = reviewElement
+                    ? reviewElement.textContent.trim()
+                    : "";
+
+                const star = article.querySelector(
+                    '[data-testid="icnStarRating"]'
+                );
+
+                const countStar = star.getAttribute("aria-label");
+
+                const intStar = parseInt(countStar.split(" ").pop());
+
+                reviewArticle.push({
+                    user,
+                    review,
+                    intStar,
+                });
+
+                count++; // Increment the counter
+            });
+
+            return reviewArticle;
+        });
+
+        // Log the length of the reviewArticle array
+        // console.log("Number of articles:", articleHTML.length);
+
+        // Close the browser instance
+        await browser.close();
+
+        // Respond with the extracted articles
+        res.status(200).json({ articles: articleHTML });
     } catch (error) {
         console.log(error);
         res.status(500).json({
             message: "Error occurred while scraping website",
+            detail: error,
         });
     }
 };
