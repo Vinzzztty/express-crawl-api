474b99a8eea72c92d7dab17f8643b24971d4d920
Vinzzztty <kevinarnandes21@gmail.com>
2024-04-10 23:04:32 +0700

    feat: refactoring code

--- routes/tokopedia/tokopedia.router.js
+++ routes/tokopedia/tokopedia.router.js
@@ -8,6 +8,5 @@ const tokped = require("./tokopedia.controller");
 // Routes
 router.route("/").get(tokped.indexTokped);
 router.route("/").post(tokped.crawlTokped);
-router.route("/2").get(tokped.indexTokped2);
 
 module.exports = router;
